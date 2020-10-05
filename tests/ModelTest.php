30bbb9df2dbc5f4a88c8f27ba477a2ffb228cfcc
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-10-05 15:01:33 +0200

    Add test to assert creation of empty array when joined table has no rows joined to the base model

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -7,6 +7,7 @@ use Expreql\Expreql\Database;
 
 use PHPUnit\Framework\TestCase;
 
+use function PHPUnit\Framework\assertCount;
 use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertIsInt;
 use function PHPUnit\Framework\assertNotNull;
@@ -95,6 +96,14 @@ class ModelTest extends TestCase
             ->join(Question::class)->execute();
 
         assertIsInt($exercise[0]->questions->count());
+    }
+
+    public function testJoinWithNoJoinedRows()
+    {
+        $exercise = Exercise::select()->join(Question::class)
+            ->where(Exercise::field('id'), 14)->execute();
+
+        assertCount(0, $exercise[0]->questions);
     }
 
     public function testFindExercise()
